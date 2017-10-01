cafce3fffeb3b06ca29cd9b537be0108e6bcd57c
Alyssa Ong <alon@microsoft.com>
2017-10-01 15:17:31 +0800

    Check that bot itself is responding

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ const ConnectionStatus = BotChat.ConnectionStatus;
 
 // Initialise directline object
 var directLine = new BotChat.DirectLine({
-    secret: 'YOUR SECRET HERE' // You probably want to replace this to use token
+    secret: 'YOUR DIRECT LINE SECRET HERE' // You probably want to replace this to use token
 });
 
 //Subscribe to activities
@@ -10,6 +10,7 @@ directLine.activity$
 .subscribe(
     activity => console.log("received activity ", activity)
 );
+
 //Monitor connection status to see if bot is online
 directLine.connectionStatus$
 .subscribe(connectionStatus => {
@@ -23,20 +24,40 @@ directLine.connectionStatus$
           break
         case ConnectionStatus.Online:           // successfully connected to the converstaion. Connection is healthy so far as we know.
           console.log("Online and ready to go!")
-          // Bot is online, so let's initialize the chat UI
-          BotChat.App({
-            directLine: { secret: 'YOUR SECRET HERE' }, // You probably want to replace this to use token 
-            user: { id: 'userid' },
-            bot: { id: 'botid' },
-            resize: 'detect'
-          }, document.getElementById("bot"));
+          //Post an activity to check if bot is answering properly
+          directLine.postActivity({
+              from: { id: 'myUserId', name: 'myUserName' }, // required (from.name is optional)
+              type: 'message',
+              text: 'a message for you, Rudy'
+          }).subscribe(
+              id => {
+                console.log("Posted activity, assigned ID ", id)
+                if (id === 'retry') {
+                  // Error 50x
+                  document.getElementById("bot").innerText = "Bot currently offline. Please try again later.";
+                } else {
+                  // Bot is online, so let's initialize the chat UI
+                  BotChat.App({
+                    directLine: { secret: 'YOUR DIRECT LINE SECRET HERE' }, // You probably want to replace this to use token 
+                    user: { id: 'userid' },
+                    bot: { id: 'botid' },
+                    resize: 'detect'
+                  }, document.getElementById("bot"));
+                }
+              },
+              error => {
+                // Error 40x
+                console.log("Error posting activity", error)
+                document.getElementById("bot").innerText = "Bot currently offline. Please try again later.";
+              }
+          );
           break
         case ConnectionStatus.ExpiredToken:     // last operation errored out with an expired token. Your app should supply a new one.
           console.log("Expired token!")
           break
         case ConnectionStatus.FailedToConnect:  // the initial attempt to connect to the conversation failed. No recovery possible.
           console.log("Totally failed to connect!")
-          document.getElementById("bot").innerText = "Bot currently offline. Please try again later.";
+          document.getElementById("bot").innerText = "Direct line channel currently offline. Please try again later.";
           break
         case ConnectionStatus.Ended:            // the bot ended the conversation
           console.log("Conversation ended.")
